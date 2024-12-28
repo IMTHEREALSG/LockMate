ca67d3cfe828fd14b31a529e4cbafc6fb146a34e
Sarthak Gadekar <sarthakgadekar03@gmail.com>
2024-12-28 16:39:56 +0530

    Added some Changes to Backend

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 
 const corsOptions={
-    origin: 'http://localhost:3000',
+    origin: 'https://lockmate.netlify.app',
     methods: 'GET,PUT,POST,DELETE',
     allowedHeaders:'Content-Type,Authorization',
     credentials:true,
