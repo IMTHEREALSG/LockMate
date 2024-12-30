45f8584a1570a0d814b73031f0597e750b20a5a7
Sarthak Gadekar <sarthakgadekar03@gmail.com>
2024-12-30 17:11:25 +0530

    Cors issue fixed

--- Backend/server.js
+++ Backend/server.js
@@ -8,22 +8,9 @@ require('./config');
 require('dotenv').config();
 const app = express();
 
-const corsOptions = {
-    origin: 'https://lockmate.netlify.app', // Replace with your frontend URL
-    methods: 'GET,PUT,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-    credentials: true,
-};
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://lockmate.netlify.app'); // Replace with your frontend URL
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../Frontend/pages')));
