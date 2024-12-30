0b3e0b276fb0e32c71002113a947f00396aeacdc
Sarthak Gadekar <sarthakgadekar03@gmail.com>
2024-12-30 16:43:34 +0530

    Cors Issue Fixed

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,14 @@ const corsOptions = {
     credentials: true,
 };
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://lockmate.netlify.app'); // Replace with your frontend URL
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
